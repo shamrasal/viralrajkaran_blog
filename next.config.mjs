3d4420b1aec25ce4cb284cd35d1522c74d268a75
shamrasal <106094203+shamrasal@users.noreply.github.com>
2025-03-30 14:16:45 +0530

    files

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: If you want to specify a custom directory for the exported static files
+  distDir: 'out',
+};
+
+export default nextConfig;
